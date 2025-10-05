e903e9784184d38faf4d1492b38e105261522f4f
davidsodadavid <samitnesvrstanih@gmail.com>
2025-10-05 15:44:15 +0200

    medusa config file

--- medusa-config.ts
+++ medusa-config.ts
@@ -14,12 +14,12 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
-    // workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
-    // redisUrl: process.env.REDIS_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+    redisUrl: process.env.REDIS_URL,
   },
   admin: {
-    // disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-    // backendUrl: process.env.MEDUSA_BACKEND_URL,
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
   modules: [
     {
@@ -27,25 +27,25 @@ module.exports = defineConfig({
       options: {},
     },
 
-    // {
-    //   resolve: "@medusajs/medusa/cache-redis",
-    //   options: {
-    //     redisUrl: process.env.REDIS_URL,
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/event-bus-redis",
-    //   options: {
-    //     redisUrl: process.env.REDIS_URL,
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/workflow-engine-redis",
-    //   options: {
-    //     redis: {
-    //       url: process.env.REDIS_URL,
-    //     },
-    //   },
-    // },
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
   ],
 })
