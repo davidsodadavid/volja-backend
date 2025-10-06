7c4f22be9aa63c33dbe6b7d43fed9f3a8c4be1a6
davidsodadavid <samitnesvrstanih@gmail.com>
2025-10-06 16:53:07 +0200

    new env variable for files url

--- medusa-config.ts
+++ medusa-config.ts
@@ -55,8 +55,7 @@ module.exports = defineConfig({
             resolve: "@medusajs/medusa/file-local",
             id: "local",
             options: {
-              upload_dir: "uploads",
-              backend_url: "https://api.ateljevolja.si/static"
+              backend_url: process.env.MEDUSA_BACKEND_FILES_URL
             },
           },
         ],
