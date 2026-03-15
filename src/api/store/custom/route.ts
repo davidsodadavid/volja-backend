68ab00bf32d930048e302cdea9cbd59e7edcddf0
boyan <boyan.dedic@gmail.com>
2026-03-15 23:06:51 +0100

    images relation

--- src/api/store/custom/route.ts
+++ src/api/store/custom/route.ts
@@ -26,6 +26,7 @@ export async function GET(req: MedusaRequest, res: MedusaResponse) {
       "title",
       "handle",
       "thumbnail",
+      "images.*",
       "variants.*",
       "custom.*",
     ],
